ab6f3e08e12ae16e1f1b8518ba46521f8ca72980
Charlie Möllbrink <charlie.mollbrink@gmail.com>
2018-03-23 21:57:44 +0100

    add videolist

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -17,7 +17,7 @@ export class App extends React.Component<AppProps, {}> {
                       <div className="container-fluid">
                         <div className="collapse navbar-collapse" id="header__navbar">
                           <ul className="nav navbar-nav">
-                            <li className="nav__item nav__item--index"><Link to="/">{this.props.name}</Link></li>
+                            <li className="nav-item nav-item--index"><Link to="/">{this.props.name}</Link></li>
                           </ul>
                         </div>
                       </div>
@@ -29,12 +29,6 @@ export class App extends React.Component<AppProps, {}> {
                           <Route path="/video/:id" component={Video}  />
                       </Switch>
                   </main>
-                  <footer>
-                     <span className="footer__contact">
-                         <span className="footer__contact__email">email@onecoolcat.io</span>
-                         <span className="footer__contact__phone">0110-010101010</span>
-                     </span>
-                  </footer>
               </div>
 
         );
--- src/components/videoList.tsx
+++ src/components/videoList.tsx
@@ -1,18 +1,32 @@
 import * as React from "react";
 import { Link } from 'react-router-dom';
+import { Video } from "./video";
 
 const videoList = require("../data/videoList.json");
 
-export interface VideoListProps { };
+export interface VideoListProps {
+  show: number
+};
 
 export class VideoList extends React.Component<VideoListProps, {}> {
   render() {
+    let rows = [];
+    //console.log(Object.entries(videoList)[items]);
+    for (let i = 0; i < videoList.items.length; i++) {
+      const props = {
+        videoUrl: 'https://www.youtube.com/watch?v=' + videoList.items[i].id,
+        thumbnail : videoList.items[i].snippet.thumbnails.medium.url
+
+      }
+
+      rows.push(<Video videoUrl={props.videoUrl} thumbnail={props.thumbnail} key={i}/>);
+    };
     return (
         <ul>
           <li>
             <Link to={"video/" + videoList.items[0].id}>
               <article>
-                <img src={videoList.items[0].snippet.thumbnails.medium.url}/>
+                {rows}
               </article>
             </Link>
           </li>
